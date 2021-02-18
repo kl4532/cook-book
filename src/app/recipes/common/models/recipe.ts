b00a717ec7176072bf54f5647ca623ff6c45e13d
kl4532 <kornellukasiak@gmail.com>
2021-02-18 16:35:31 +0100

    init models

---
+++ src/app/recipes/common/models/recipe.ts
@@ -0,0 +1,9 @@
+import {Ingredient} from './ingredient';
+
+export interface Recipe {
+  _id: string;
+  name: string;
+  preparationTimeInMinutes: number;
+  description: string;
+  ingredients: Ingredient[];
+}
