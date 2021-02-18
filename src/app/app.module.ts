8e8441e3ac2c86473c2701c640551ed13cc6294b
kl4532 <kornellukasiak@gmail.com>
2021-02-18 20:06:38 +0100

    add imports/exports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {RecipesModule} from './recipes/recipes.module';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AuthorInfoDialogComponent } from './shared/author-info-dialog/author-info-dialog.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+import { ConfirmDialogComponent } from './shared/confirm-dialog/confirm-dialog.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthorInfoDialogComponent
+    AuthorInfoDialogComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     RecipesModule,
-    MatDialogModule
+    // MatModules
+    MatDialogModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -3,13 +3,35 @@ import { CommonModule } from '@angular/common';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import {RouterModule} from '@angular/router';
+import {RecipeEditComponent} from './recipe/recipe-edit/recipe-edit.component';
+import {RecipeCreateComponent} from './recipe/recipe-create/recipe-create.component';
+import {FormsModule} from '@angular/forms';
+// MatImports
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
-  declarations: [RecipeComponent, RecipeListComponent, RecipeDetailsComponent],
+  declarations: [RecipeComponent, RecipeListComponent, RecipeDetailsComponent, RecipeEditComponent, RecipeCreateComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    RouterModule,
+    // MatModules
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatButtonModule,
+
   ],
-  exports: [RecipeListComponent, RecipeListComponent, RecipeDetailsComponent]
+  exports: [
+    RecipeListComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipeEditComponent,
+    RecipeCreateComponent]
 })
 export class RecipesModule { }
