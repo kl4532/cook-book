e3c4bdf354aa50f421c4fa1ca0eccd23b19d2e55
kl4532 <kornellukasiak@gmail.com>
2021-02-18 20:04:16 +0100

    add initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RecipeDetailsComponent} from './recipes/recipe-details/recipe-details.component';
+import {RecipeEditComponent} from './recipes/recipe/recipe-edit/recipe-edit.component';
+import {RecipeCreateComponent} from './recipes/recipe/recipe-create/recipe-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'recipe-details/:id',
+    component: RecipeDetailsComponent,
+  },
+  {
+    path: 'recipe-edit/:id',
+    component: RecipeEditComponent,
+  },
+  {
+    path: 'recipe-create',
+    component: RecipeCreateComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
