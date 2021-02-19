8e943331c8c25cf28e3aecdffdfbcf80aae511e3
kl4532 <kornellukasiak@gmail.com>
2021-02-19 02:24:31 +0100

    add get functions to fetch mock data

--- src/app/recipes/common/recipe.service.ts
+++ src/app/recipes/common/recipe.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import {Recipe} from './models/recipe';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {ConfirmDialogComponent} from '../../shared/confirm-dialog/confirm-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class RecipeService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    public dialog: MatDialog) { }
+
+  getAllRecipes(): any {
+    return this.http.get('../../assets/mockRecipes.json').pipe(map((obj: any) => {
+      return obj.recipes;
+    }));
+  }
+
+  getRecipeDetails(id: string): any{
+    // return this.http.get(`recipes/${id}`);
+    return this.http.get('../../assets/mockRecipes.json').pipe(map((obj: any) => {
+      return obj.recipes.filter((recipe: Recipe) => recipe._id === id)[0];
+    }));
+  }
+
+  confirmDeletion(id: string): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        message: `Are you sure want to recipe ${id}`,
+        buttonText: {
+          ok: 'Yes',
+          cancel: 'No'
+        }
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        // remove recipe with id
+        console.log('deletion confirmed');
+      }
+    });
+  }
 }
